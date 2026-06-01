c4dd85c401073a8e675b03767e551fee2350ba29
junkun.yuan <junkun.yuan@bytedance.com>
2026-06-01 02:53:41 +0000

    update

--- src/nexus_align/cli/main.py
+++ src/nexus_align/cli/main.py
@@ -24,21 +24,42 @@ def main(cfg, env):
     # --------------------------------------------------------------------------------
     # 1. Prepare dataset
     # --------------------------------------------------------------------------------
-    dataset = registry.get("dataset", cfg.data.name)(
+    train_batch_size = cfg.algorithm.train.train_batch_size
+    grad_accu_step = cfg.algorithm.train.grad_accu_step
+    sample_ratio = cfg.data.sample_ratio
+    drop_last = cfg.data.drop_last
+    cache_dir = cfg.data.cache_dir
+
+    train_dataset = registry.get("dataset", cfg.data.name)(
         cfg.data.lmdb_path,
         flip_prob=cfg.data.flip_prob,
     )
     sampler = torch.utils.data.distributed.DistributedSampler(
-        dataset, num_replicas=world_size, rank=rank, shuffle=True,
+        train_dataset, num_replicas=world_size, rank=rank, shuffle=True,
     )
     dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=cfg.data.batch_size // world_size,
+        train_dataset,
+        batch_size=train_batch_size // world_size,
         sampler=sampler,
         num_workers=cfg.data.num_workers,
         pin_memory=True,
-        drop_last=True,
+        drop_last=drop_last,
     )
+    coef = world_size * grad_accu_step
+    total_train_batch_size = train_batch_size * coef
+    info = ["\n📚 Training data:"]
+    info += [f"    dataset: {cfg.data.name}"]
+    info += [f"    sample count: {len(train_dataset)}"]
+    info += [f"    batchsize: {train_batch_size}"]
+    info += [f"    total batchsize: {total_train_batch_size}"]
+    if sample_ratio != 1.:
+        info += [f"    sample_ratio: {sample_ratio}"]
+    if drop_last:
+        info += ["    drop_last: true"]
+    if cache_dir:
+        info += [f"    cache_dir: {cache_dir}"]
+    print("\n".join(info))
+    print("✅ Prepared training dataset")
 
     # --------------------------------------------------------------------------------
     # 2. Prepare models
@@ -48,6 +69,7 @@ def main(cfg, env):
         num_classes=cfg.model.num_classes,
         use_cfg=cfg.model.cfg_prob > 0,
     ).to(device)
+    print("✅ Prepared model")
     # --------------------------------------------------------------------------------
     # 3. Prepare algorithms
     # --------------------------------------------------------------------------------
